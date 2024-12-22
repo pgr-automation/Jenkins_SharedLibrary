7351bdebc422523720f43fc7ae7f661825fe7a9c
pgr-automation <grprashanth94@gmail.com>
2024-12-23 05:22:01 +0530

    pm2 monitor

--- vars/kubernetesAgent.groovy
+++ vars/kubernetesAgent.groovy
@@ -1,23 +1,5 @@
-def call(String jenkinsUrl = "http://192.168.1.120:8070", String appType = "java") {
-    // Define default image for each application type from Docker Hub
-    def image = ""
-    switch (appType) {
-        case "java":
-            image = "openjdk:11-jdk-slim"  // Official OpenJDK image for Java applications
-            break
-        case "nodejs":
-            image = "node:16-slim"  // Official Node.js image
-            break
-        case "python":
-            image = "python:3.9-slim"  // Official Python image
-            break
-        case "springboot":
-            image = "openjdk:11-jdk-slim"  // Using OpenJDK for Spring Boot
-            break
-        default:
-            image = "jenkins/inbound-agent:latest"  // Default Jenkins inbound agent image
-    }
-
+// vars/kubernetesAgent.groovy
+def call(String jenkinsUrl = "http://192.168.1.120:8070") {
     return """
 apiVersion: v1
 kind: Pod
@@ -27,7 +9,7 @@ metadata:
 spec:
   containers:
   - name: jnlp
-    image: ${image}
+    image: jenkins/inbound-agent:latest
     imagePullPolicy: Always
     args: ["\$(JENKINS_SECRET)", "\$(JENKINS_NAME)"]
     env:
