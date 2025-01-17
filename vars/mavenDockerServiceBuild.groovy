b9a7d88cffb662e47b9025d3f0cfefe9e6d1b5e1
pgr-automation <grprashanth94@gmail.com>
2025-01-17 19:06:41 +0530

    groovy

--- vars/mavenDockerServiceBuild.groovy
+++ vars/mavenDockerServiceBuild.groovy
@@ -5,6 +5,7 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    //
 
     // Ensure required parameters are provided
     def cloudAgent = config.buildAgentCluster
