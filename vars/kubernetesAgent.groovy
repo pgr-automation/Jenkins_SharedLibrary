f8c8d9b7ec3b4ff3f27c4dad347e730d3e79d9c7
pgr-automation <grprashanth94@gmail.com>
2025-01-23 18:35:36 +0530

    jenkins

--- vars/kubernetesAgent.groovy
+++ vars/kubernetesAgent.groovy
@@ -4,55 +4,50 @@ apiVersion: v1
 kind: Pod
 metadata:
   labels:
-    jenkins: k8-jenkins
+    jenkins: "k8-jenkins"
 spec:
   containers:
-  - name: jnlp
-    image: jenkins/inbound-agent:latest
-    imagePullPolicy: Always
-    args: ["\$(JENKINS_SECRET)", "\$(JENKINS_NAME)"]
-    env:
-    - name: JENKINS_URL
-      value: "${jenkinsUrl}"
-    - name: JAVA_OPTS
-      value: "-Djenkins.slave.agents.disableSslVerification=true"
-    resources:
-      requests:
-        cpu: "100m"
-        memory: "256Mi"
-      limits:
-        cpu: "500m"
-        memory: "512Mi"
-    volumeMounts:
-    - name: workspace-volume
-      mountPath: /home/jenkins/agent
-  - name: maven
-    image: maven:3.8.5-openjdk-17
-    imagePullPolicy: Always
+  - name: "docker-agent"
+    image: "docker:latest"
     command:
-    - cat
+      - "cat"
     tty: true
+    securityContext:
+      privileged: true
+    resources:
+      limits:
+        memory: "128Mi"
+        cpu: "500m"
     volumeMounts:
-    - name: workspace-volume
-      mountPath: /home/jenkins/agent
-    - name: docker-sock
-      mountPath: /var/run/docker.sock
-      
-  volumes:
-  - name: workspace-volume
-    emptyDir: {}
-  - name: docker-sock
-    hostPath:
-      path: /var/run/docker.sock
+    - name: "workspace-volume"
+      mountPath: "/home/jenkins/agent"
+    - name: "docker-sock"
+      mountPath: "/var/run/docker.sock"
+  
+  - name: "hadolint-agent"
+    image: "hadolint/hadolint:v2.12.0-alpine"
+    tty: true
+    securityContext:
+      privileged: true
+    resources:
+      limits:
+        memory: "128Mi"
+        cpu: "500m"
+    volumeMounts:
+    - name: "workspace-volume"
+      mountPath: "/home/jenkins/agent"
+    - name: "docker-sock"
+      mountPath: "/var/run/docker.sock"
+  
   nodeSelector:
-    kubernetes.io/os: linux
-  tolerations:
-  - key: "key"
-    operator: "Equal"
-    value: "value"
-    effect: "NoSchedule"
-  dnsPolicy: ClusterFirst
+    kubernetes.io/os: "linux"
+  restartPolicy: "Never"
+  serviceAccount: "jenkins"
+  volumes:
+  - name: "workspace-volume"
+    emptyDir: {}
+  - name: "docker-sock"
+    hostPath:
+      path: "/var/run/docker.sock"
 """
 }
-
-##
